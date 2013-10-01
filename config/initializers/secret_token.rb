61893cb1be3933b52caeebd2f899281fa1c0ff79
Ben Ettinger <ben.ettinger@gmail.com>
2013-09-30 22:45:30 -0400

    First commit and README update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pill::Application.config.secret_token = 'b5800abbd471c6083c06b5ca48482353d1576ecf857cf6a277500233d143a616aee71fbcf53e329d7f6f1a3042bfca61989f855333e3917f207c1758c8e50bc2'
