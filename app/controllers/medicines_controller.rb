af4296882ab36b98d420784703a2cca7804c6dde
Ben Ettinger <ben.ettinger@gmail.com>
2013-10-15 14:22:35 -0400

    View, Add, Edit medicines

--- app/controllers/medicines_controller.rb
+++ app/controllers/medicines_controller.rb
@@ -1,6 +1,6 @@
 class MedicinesController < ApplicationController
   def index
-    @medicines = Medicine.all
+    @medicine = Medicine.all
   end
 
   def show
@@ -8,8 +8,33 @@ class MedicinesController < ApplicationController
   end
 
   def new
+    @medicine = Medicine.new
   end
 
-  def edit
+  def create
+  @medicine = Medicine.new(params[:medicine])
+  #raise
+  if @medicine.save
+    flash[:notice] = "Entry was saved."
+    redirect_to @medicine
+  else
+    flash[:error] = "There was an error saving the entry. Please try again."
+    render :new
   end
+end
+
+def edit
+  @medicine = Medicine.find(params[:id])
+end
+
+  def update
+        @medicine = Medicine.find(params[:id])
+    if @medicine.update_attributes(params[:medicine])
+      flash[:notice] = "Entry was updated."
+      redirect_to @medicine
+    else
+      flash[:error] = "There was an error saving the entry. Please try again."
+      render :new
+  end
+end
 end
